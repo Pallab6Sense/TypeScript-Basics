7e009fd1e72c0411013d01fbc412fd10d3b6ceaf
Pallab6Sense <pallab@6sensehq.com>
2023-03-04 18:12:13 +0600

    Typeacript functions

--- dist/function.js
+++ dist/function.js
@@ -1,8 +1,14 @@
 "use strict";
-function calculareTax(income) {
-    if (income < 50000)
+function calculareTax(income, taxYear) {
+    if (taxYear < 2022 && income < 50000)
         return income * 1.2;
     return income * 1.3;
 }
-console.log(calculareTax(40000));
+function calculareTax1(income, taxYear = 2022) {
+    if ((taxYear || 2022) < 2022 && income < 50000)
+        return income * 1.2;
+    return income * 1.3;
+}
+console.log(calculareTax(40000, 2022));
+console.log(calculareTax1(40000, 2020));
 //# sourceMappingURL=function.js.map
