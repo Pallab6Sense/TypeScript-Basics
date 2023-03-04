0ffa70360d618c57d74b426fa62b6f4ead45231c
Pallab6Sense <pallab@6sensehq.com>
2023-03-04 17:34:04 +0600

    TypeScript Array

---
+++ dist/array.js
@@ -0,0 +1,6 @@
+"use strict";
+let num = [1, 2, 3, 4];
+const newNumner = num.map((num) => {
+    console.log(num);
+});
+//# sourceMappingURL=array.js.map
---
+++ src/array.ts
@@ -0,0 +1,5 @@
+let num: number[] = [1, 2, 3, 4];
+
+const newNumner = num.map((num) => {
+  console.log(num);
+});
