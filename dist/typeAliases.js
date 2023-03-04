0f8b0edb7ce417e8bd81f8ba050fb3bd1b4c0068
Pallab6Sense <pallab@6sensehq.com>
2023-03-05 00:29:30 +0600

    TypeScript TypeAliases

---
+++ dist/typeAliases.js
@@ -0,0 +1,10 @@
+"use strict";
+let employee = {
+    id: 1,
+    name: 'Pallab',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+employee;
+//# sourceMappingURL=typeAliases.js.map
---
+++ src/typeAliases.ts
@@ -0,0 +1,16 @@
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: 'Pallab',
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+employee
+
