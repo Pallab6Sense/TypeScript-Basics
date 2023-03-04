2a64a7634f638ceb0dd0182f2438864b540f420f
Pallab6Sense <pallab@6sensehq.com>
2023-03-04 18:13:58 +0600

    Typeacript functions

--- src/function.ts
+++ src/function.ts
@@ -7,7 +7,7 @@ function calculareTax1(income: number, taxYear = 2022): number {
   //! taxYear?: number   taxYear=2022
   //!taxyear is a optional parameter here.we can fix the optional
   //!parameter by give a default value.but the better practice is to give a  default value in the function parameter.
-  if ((taxYear || 2022) < 2022 && income < 50_000) return income * 1.2;
+  if (taxYear < 2022 && income < 50_000) return income * 1.2;
   return income * 1.3;
 }
 
