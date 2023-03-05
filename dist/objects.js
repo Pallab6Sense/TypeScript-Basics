2a155a1eedd4e84b36c29b31cd7398204925b240
Pallab6Sense <pallab@6sensehq.com>
2023-03-05 14:24:53 +0600

    TypeScript object

---
+++ dist/objects.js
@@ -0,0 +1,14 @@
+"use strict";
+let person1 = {
+    name: 'pallab',
+    age: 23,
+    address: 'Mirpur-2,Dhaka',
+};
+console.log(person1);
+person1 = {
+    name: 'x',
+    age: 22,
+    address: 'jashore',
+};
+console.log(person1);
+//# sourceMappingURL=objects.js.map
---
+++ src/objects.ts
@@ -0,0 +1,17 @@
+let person1 = {
+  name: 'pallab',
+  age: 23,
+  address: 'Mirpur-2,Dhaka',
+};
+
+console.log(person1);
+
+//!We can redeclare this object again but the structure has to be the same.
+
+person1 = {
+  name: 'x',
+  age: 22,
+  address: 'jashore',
+};
+
+console.log(person1);
